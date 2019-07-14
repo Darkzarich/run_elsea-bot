b398c9baf02305bad4d6ca3c130e08be4c12ce51
Dark_zarich <darkzarichv2@gmail.com>
2019-07-14 16:16:08 +0300

    trying to solve rerun bug

--- src/services/grabber.js
+++ src/services/grabber.js
@@ -37,6 +37,7 @@ function errorLogWrapper(e, debug=true) {
 const grab = (driver, command) => {
 	return new Promise( (resolve, reject) => {
 	    try {
+	    	console.log(command)
 	        Object.keys(command).forEach( el => {
 	            driver.findElement(By.id(el)).then(field => {
 	                if (command[el] == true) {
